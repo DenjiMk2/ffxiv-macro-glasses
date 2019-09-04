cd0e367f3de52e6d87eb426a3c7a60afa014d4cd
Denji_mk2 <13076835+DenjiMk2@users.noreply.github.com>
2019-09-04 22:16:25 +0900

    fast commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ffxiv-macro-glasses',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
